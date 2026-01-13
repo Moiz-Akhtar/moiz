5e02516bbfcfd6296a62fb4fce0ed411e6bf9c5a
Moiz-Akhtar <59883618+Moiz-Akhtar@users.noreply.github.com>
2026-01-13 18:19:15 +0500

    commit

--- src/app/components/home/hero-section/index.tsx
+++ src/app/components/home/hero-section/index.tsx
@@ -8,20 +8,21 @@ const index = () => {
                     <div className="flex flex-col gap-4 md:gap-7 max-w-2xl">
                         <div>
                             <div className="flex items-center gap-8">
-                                <h1>I'm Sruthi</h1>
+                                <h1>Hi I'm Moiz</h1>
                                 <div className="wave">
                                 <Image src={"/images/home/banner/wave-icon.svg"} alt="wave-icon" width={62} height={62} className=""/>
                                 </div>
                             </div>
-                            <h1>UI/UX Designer</h1>
+                            <h1>Website Developer</h1>
+                            <h2>Wordpress, Shopify, Frontend, SEO</h2>
                         </div>
-                        <p className="text-secondary font-normal max-w-md xl:max-w-xl">Lorem Ipsum is simply dummy text of the printing and typesetting industry. variations of passages of Lorem Ipsum available, but the majority have suffered alteration</p>
+                        <p className="text-secondary font-normal max-w-md xl:max-w-xl">I ensure businesses flourish by creating websites that are beautiful as fast. More than 6 years of creating the most trusted platforms on the web. I focus on transforming imaginative ideas into high speed, scalable and aesthetically refined digital products enabling companies to be at the forefront in their industry.</p>
                     </div>
-                    <Image src={"/images/home/banner/banner-img.png"} alt="banner-img" width={685} height={650} className="block lg:hidden" />
+                    <Image src={"/images/home/banner/banner-img-moiz.png"} alt="banner-img" width={685} height={650} className="block lg:hidden" />
                 </div>
             </div>
             <div className="absolute right-0 top-0 hidden h-auto w-1/2 lg:block 2xl:h-171.5 2xl:w-187.5">
-                <Image src={"/images/home/banner/banner-img.png"} alt="banner-img" width={685} height={650} className=" absolute right-0 top-0 z-1" />
+                <Image src={"/images/home/banner/banner-img-moiz.png"} alt="banner-img" width={685} height={650} className=" absolute right-0 top-0 z-1" />
             </div>
         </section>
     )
